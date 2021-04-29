4c52fce9135909682bdb22713e726776637f4a0a
Wawrzyn321 <Wawrzyn321@gmail.com>
2021-04-29 09:02:44 +0200

    Finish minibump

--- node/index.js
+++ node/index.js
@@ -1,7 +1,7 @@
 const git = require("nodegit");
 const execa = require("execa");
-const fs = require("fs");
 const yaml = require("js-yaml");
+const fs = require("fs");
 
 const CORE_UI = "core-ui";
 const CORE = "core";
@@ -12,23 +12,23 @@ const BUSOLA_PATH = "/Users/i515358/Sites/kyma-project/busola";
 
 const fileChangeMappings = [
   {
-    prefix: "core-ui",
+    prefix: "core-ui/",
     bump: [CORE_UI],
   },
   {
-    prefix: "core",
+    prefix: "core/",
     bump: [CORE],
   },
   {
-    prefix: "service-catalog-ui",
+    prefix: "service-catalog-ui/",
     bump: [CATALOG],
   },
   {
-    prefix: "backend",
+    prefix: "backend/",
     bump: [BACKEND],
   },
   {
-    prefix: "shared",
+    prefix: "shared/",
     bump: [CORE_UI, CATALOG],
   },
 ];
@@ -36,7 +36,7 @@ const fileChangeMappings = [
 const bumpMappings = {
   [CORE_UI]: {
     filePath: BUSOLA_PATH + "/resources/web/deployment.yaml",
-    registryPath: "eu.gcr.io/kyma-project/busola-backend:PR-",
+    registryPath: "eu.gcr.io/kyma-project/busola-core-ui:PR-",
     update: (content, image) => {
       content.spec.template.spec.containers.find(
         (c) => c.name === "core-ui"
@@ -75,24 +75,31 @@ const getPRNumber = async () => {
     });
     const pr = JSON.parse(stdout).currentBranch;
     if (!pr) {
-      console.log("Looks like there's no PR for this branch. Sorry.");
+      console.log("Looks like there's no PR for this branch. Create a PR and try again.");
       process.exit(1);
     } else {
       return pr.url.substr(pr.url.lastIndexOf("/") + 1);
     }
   } catch (e) {
-    console.log("Whoops, cannot get PR number. Is gh installed?");
+    console.log("Whoops, cannot get PR number. Is gh @>1.9.2 installed?");
     console.warn(e.message);
     process.exit(1);
   }
 };
 
+async function getChangedFilePaths() {
+  const repo = await git.Repository.open(BUSOLA_PATH);
+  const mainCommit = await repo.getBranchCommit('upstream/main');
+  const mainTree = await mainCommit.getTree();
+  const diff = await git.Diff.treeToWorkdirWithIndex(repo, mainTree);
+  const patches = await diff.patches();
+  return patches.map(p => p.newFile().path());
+}
+
 (async () => {
   const prNumber = process.argv.slice(2)[0] || (await getPRNumber());
 
-  const repo = await git.Repository.open(BUSOLA_PATH);
-  const status = await repo.getStatus();
-  const filePaths = status.map((s) => s.path());
+  const filePaths = await getChangedFilePaths();
 
   const toBump = new Set();
   for (const filePath of filePaths) {
